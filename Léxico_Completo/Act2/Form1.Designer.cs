e231dbb079ba43f24538351c7dbc790eef387710
N4Y3L1 <58003310+N4Y3L1@users.noreply.github.com>
2021-09-07 20:55:47 -0500

    Add files via upload

---
+++ Léxico_Completo/Act2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Act2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.original = new System.Windows.Forms.TextBox();
+            this.copia = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabla = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // original
+            // 
+            this.original.Location = new System.Drawing.Point(47, 24);
+            this.original.Multiline = true;
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(197, 116);
+            this.original.TabIndex = 0;
+            // 
+            // copia
+            // 
+            this.copia.Location = new System.Drawing.Point(355, 24);
+            this.copia.Multiline = true;
+            this.copia.Name = "copia";
+            this.copia.Size = new System.Drawing.Size(197, 116);
+            this.copia.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabla
+            // 
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Location = new System.Drawing.Point(27, 162);
+            this.tabla.Name = "tabla";
+            this.tabla.Size = new System.Drawing.Size(542, 150);
+            this.tabla.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 335);
+            this.Controls.Add(this.tabla);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.copia);
+            this.Controls.Add(this.original);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox original;
+        private System.Windows.Forms.TextBox copia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView tabla;
+    }
+}
+
---
+++ Léxico_Completo/Act2/Form1.cs
@@ -0,0 +1,445 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string frase = original.Text;
+            int indice = 0;
+            int estado = 0;
+            int estadoFinal = -1;
+            string lexema = "";
+            string token;
+           
+            tabla.Columns.Add("ID", "ID");
+            tabla.Columns.Add("Lexema", "Lexema");
+            tabla.Columns.Add("Token", "Token");
+
+            while ((indice <= (frase.Length - 1)) && (estadoFinal == -1))
+            {
+                lexema = " ";
+                token = "error";
+                while ((indice <= (frase.Length - 1)) && (estadoFinal != 25))
+                {
+                    if (estadoFinal == -1)
+                    {
+                        if (char.IsWhiteSpace(frase[indice]))
+                        {
+                            estadoFinal = -1;
+                        }
+                        else if (char.IsLetter(frase[indice]) || frase[indice] == '_')
+                        {
+                            estado = 0;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "identificador";
+                        }
+                        else if (char.IsDigit(frase[indice]))
+                        {
+                            estado = 1;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "entero";
+                        }
+                        else if (frase[indice] == '+' || frase[indice] == '-')
+                        {
+                            estado = 5;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opSuma";
+                        }
+                        else if (frase[indice] == '/' || frase[indice] == '*')
+                        {
+                            estado = 6;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opMultiplicación";
+                        }
+                        else if (frase[indice] == '<' || frase[indice] == '>')
+                        {
+                            estado = 7;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opRelacional";
+                        }
+                        else if (frase[indice] == '|')
+                        {
+                            estado = 8;
+
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "error";
+                        }
+                        else if (frase[indice] == '&')
+                        {
+                            estado = 9;
+
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "error";
+                        }
+                        else if (frase[indice] == '!')
+                        {
+                            estado = 10;
+
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opNot";
+                        }
+                        else if (frase[indice] == '=')
+                        {
+                            estado = 18;
+
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "igual";
+                        }
+                        else if (frase[indice] == ';')
+                        {
+                            estado = 12;
+                            estadoFinal = estado;
+
+                            lexema += frase[indice];
+                            token = "punto y coma";
+                        }
+                        else if (frase[indice] == ',')
+                        {
+                            estado = 13;
+                            estadoFinal = estado;
+
+                            lexema += frase[indice];
+                            token = "coma";
+                        }
+                        else if (frase[indice] == '(')
+                        {
+                            estado = 14;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "parentesis izquierdo";
+                        }
+                        else if (frase[indice] == ')')
+                        {
+                            estado = 15;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "parentesis derecho";
+                        }
+                        else if (frase[indice] == '{')
+                        {
+                            estado = 16;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "corchete izquierdo";
+                        }
+                        else if (frase[indice] == '}')
+                        {
+                            estado = 17;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "corchete derecho";
+                        }
+                        else if (frase[indice] == '$')
+                        {
+                            estado = 23;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "pesos";
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                            lexema = frase[indice].ToString();
+                            token = "error";
+                        }
+                        indice++;
+                    }
+                    else if (estadoFinal == -1)
+                    {
+                        estadoFinal = 25; 
+                    }
+                    else if (estadoFinal == 0)
+                    {
+                        if (char.IsLetter(frase[indice]) || frase[indice] == '_')
+                        {
+                            estado = 0;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "identificador";
+                            indice++;
+                        }
+                        else if (char.IsDigit(frase[indice]))
+                        {
+                            estado = 0;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "identificador";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                        }
+                    }
+                    else if (estadoFinal == 1)
+                    {
+                        if (char.IsDigit(frase[indice]))
+                        {
+                            estado = 1;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "entero";
+                            indice++;
+                        }
+                        else if (frase[indice] == '.')
+                        {
+                            estado = 24;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "error";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                        }
+                    }
+                    else if(estadoFinal == 5)
+                    {
+                        estadoFinal = 25;   
+                    }
+                    else if (estadoFinal == 6)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 7)
+                    {
+                        if (frase[indice] == '=')
+                        {
+                            estado = 7;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opRelacional";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+
+                        }
+                    }
+                    else if (estadoFinal == 8)
+                    {
+                        if (frase[indice] == '|')
+                        {
+                            estado = 8;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opOr";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+
+                        }
+                    }
+                    else if (estadoFinal == 9)
+                    {
+                        if (frase[indice] == '&')
+                        {
+                            estado = 9;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opAnd";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+
+                        }
+                    }
+                    else if (estadoFinal == 10)
+                    {
+                        if (frase[indice] == '=')
+                        {
+                            estado = 9;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opIgualdad";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                        }
+                    }
+                    else if (estadoFinal == 18)
+                    {
+                        if (frase[indice] == '=')
+                        {
+                            estado = 9;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "opIgualdad";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                        }
+                    }
+                    else if (estadoFinal == 12)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 13)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 14)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 15)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 16)
+                    {
+                        if (char.IsLetter(frase[indice]) || frase[indice] == '_')
+                        {
+                            estado = 1;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "id";
+                            indice++;
+
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                        }
+                    }
+                    else if (estadoFinal == 17)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 23)
+                    {
+                        estadoFinal = 25;
+                    }
+                    else if (estadoFinal == 24)
+                    {
+                        if (char.IsDigit(frase[indice]))
+                        {
+                            estado = 1;
+                            estadoFinal = estado;
+                            lexema += frase[indice];
+                            token = "Numero real";
+                            indice++;
+                        }
+                        else
+                        {
+                            estadoFinal = 25;
+                        }
+                    }
+
+                }
+
+                if (lexema == " if")
+                {
+                    estado = 19;
+                    estadoFinal = estado;
+                    token = "IF";
+                    indice++;
+                }
+                else if (lexema == " while")
+                {
+                    estado = 20;
+                    estadoFinal = estado;
+                    token = "While";
+                    indice++;
+                }
+                else if (lexema == " return")
+                {
+                    estado = 21;
+                    estadoFinal = estado;
+                    token = "Return";
+                    indice++;
+                }
+                else if (lexema == " else")
+                {
+                    estado = 22;
+                    estadoFinal = estado;
+                    token = "Else";
+                    indice++;
+                }
+                else if ((lexema == " int") || (lexema == " float") || (lexema == " void"))
+                {
+                    estado = 4;
+                    token = "Tipo de Dato";
+                    indice++;
+                }
+
+                tabla.Rows.Add(estado, lexema, token);
+                estadoFinal = -1;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Pruebas:
+
+int main(){
+
+int isla = 0;
+
+if (a==b){
+  cout<<"Hola, Mundo$"<<endl;
+}else{
+  cout<<"Adios, Mundo$"<<endl;
+}
+
+while( (a==b) && (c==d) ){
+  cout<<"Ciclo"<<endl;
+  a = b+c;
+  a = b-c;
+  a = b*c;
+  a = b/c;
+}
+
+}
+return 0;
+}*/
